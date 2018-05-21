01995ed456f75c806e1d66cdb5888d1bd874ee4d
5730289021-NN <norawit.n@student.chula.ac.th>
2018-05-21 23:48:09 +0700

    fix bug

---
+++ 3-Phase-Sine-Arduino.ino
@@ -0,0 +1,358 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+// 3 Phase PWM sine
+// (c) 2018 Norawit Nangsue
+// Fixed comments from
+// (c) 2016 C. Masenas
+// Modified from original DDS from: 
+// KHM 2009 /  Martin Nawrath
+
+// table of 256 sine values / one sine period / stored in flash memory
+PROGMEM const unsigned short SINE_VALUE[]  = {0,25,50,75,100,125,150,175,200,224,249,273,297,321,345,369,392,415,438,460,483,505,526,548,569,590,610,630,650,669,688,706,724,741,759,775,791,807,822,837,851,865,878,891,903,914,925,936,946,955,964,972,979,986,993,998,1004,1008,1012,1015,1018,1020,1022,1023,1023,1023,1022,1020,1018,1015,1012,1008,1003,998,992,985,978,971,963,954,944,934,924,913,901,889,876,863,849,835,820,805,789,773,756,739,722,704,685,666,647,627,607,587,566,545,524,502,480,458,435,412,389,366,342,318,294,270,246,221,197,172,147,122,97,72,47,22, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+
+
+const int HALF_WAVE    = 0;
+const int FULL_WAVE    = 1;
+const int THREE_PHASE  = 2;
+const int FAULT        = 3;
+volatile int mode = THREE_PHASE;
+
+
+volatile float scalingFactor = 1;
+
+int inputFreq = 50;
+
+float currentRead = 0;
+
+volatile float freq=1;
+const float refclk = 30.547  ;     //16 MHz/1023/2/256
+
+
+// variables used inside interrupt service declared as voilatile
+volatile unsigned long sigma;   // phase accumulator
+volatile unsigned long delta;  // phase increment
+byte phaseX, phaseY, phaseZ, phaseIX, phaseIY, phaseIZ;
+
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup()
+{
+  Serial.begin(9600);        // connect to the serial port
+  Serial.println("DDS Test");
+
+  lcd.begin();
+  lcd.backlight();
+
+  //pinMode(enablePin, OUTPUT);      // sets the digital pin as output
+  //pinMode(testPin, OUTPUT);      // sets the digital pin as output
+  pinMode(5, OUTPUT);     // pin9= PWM  output / frequency output
+  pinMode(2, OUTPUT);     // pin10= PWM  output / frequency output
+  pinMode(3, OUTPUT);     // pin11= PWM  output / frequency output
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+
+  //Mode
+  pinMode(30, INPUT); //Half wave
+  pinMode(32, INPUT); //Full wave
+  pinMode(34, INPUT); //3 phase
+
+  setMode_LCD(5);
+  
+  while(true)
+  {
+    if(analogRead(2) < 768)
+      break;
+  }
+
+  
+  
+  //setCurrent_LCD(current++);
+  //setVoltage_LCD(voltage++);
+  //setFrequency_LCD(freq);    
+    
+  Setup_Timer5();
+  Setup_Timer3();
+  Setup_Timer4();
+}
+  //digitalWrite(enablePin, HIGH);
+
+// the waveform index is the highest 8 bits of sigma
+// choose refclk as freq to increment the lsb of the 8 highest bits
+//    for every call to the ISR of timer2 overflow
+// the lsb of the 8 highest bits is 1<<24 (1LL<<24 for long integer literal)
+  
+void loop(){
+  //Frequency
+  inputFreq = 140.0 * analogRead(5)/1023.0 + 10;
+  changeFreq(inputFreq);
+  setFrequency_LCD(freq);
+
+  //Voltage
+  scalingFactor = analogRead(7)/1023.0;
+  //voltageA2D = analogRead(7);
+  
+  switch(mode){
+    case HALF_WAVE:
+    {
+      setVoltage_LCD(scalingFactor * 199.18);
+      break;
+    }
+    case FULL_WAVE:
+    {
+      setVoltage_LCD(scalingFactor * 398.37);
+      break;
+    }
+    case THREE_PHASE:
+    {
+      setVoltage_LCD(scalingFactor * 199.18);
+      break;
+    }
+    case FAULT:
+    {
+      setVoltage_LCD(0);
+      break;
+    }
+  }
+  
+  //Measured Current
+  float currentRead = (analogRead(0)-511) * 5 /1023/0.0645;
+  setCurrent_LCD(currentRead);
+
+  //Mode
+  if(analogRead(2) > 768){
+    mode = FAULT;
+  }else if(digitalRead(30)){
+    mode = HALF_WAVE;
+  }else if(digitalRead(34)){
+    mode = THREE_PHASE;
+  }else if(digitalRead(32)){
+    mode = FULL_WAVE;
+  }
+  setMode_LCD(mode);
+
+  //Serial Print
+  Serial.print("Voltage Level : ");
+  Serial.println(scalingFactor);
+  Serial.print("Frequency : ");
+  Serial.println(inputFreq);
+  delay(50);
+}
+
+
+void setMode_LCD(int mode) {
+  lcd.setCursor(0, 0);
+  switch(mode){
+    case HALF_WAVE:
+    {
+      lcd.print("Mode: Half Wave   ");
+      break;
+    }
+    case FULL_WAVE:
+    {
+      lcd.print("Mode: Full Wave   ");
+      break;
+    }
+    case THREE_PHASE:
+    {
+      lcd.print("Mode: 3 Phase    ");
+      break;
+    }
+    case FAULT:
+    {
+      lcd.print("FAULT        ");
+      break;
+    }
+    default:
+    {
+      lcd.print("Press Button");
+      lcd.setCursor(0, 1);
+      lcd.print("to Start");
+      break;
+    }
+  }
+}
+
+void setVoltage_LCD(float voltage){
+  lcd.setCursor(0, 1);
+  lcd.print("Voltage: ");
+  lcd.print(String(voltage, 2));
+  lcd.print(" V   ");
+}
+
+void setCurrent_LCD(float current){
+  lcd.setCursor(0, 2);
+  lcd.print("Current: ");
+  lcd.print(String(current, 2));
+  lcd.print(" mA  ");
+}
+
+void setFrequency_LCD(int freq){
+  lcd.setCursor(0, 3);
+  lcd.print("Frequency: ");
+  lcd.print(String(freq));
+  lcd.print(" Hz  ");
+}
+
+
+
+void changeFreq(float _freq){
+  cbi (TIMSK5,TOIE5);              // disable timer5 overflow detect
+  freq = _freq;
+  delta=(1LL<<24)*freq/refclk;  // update phase increment
+  sbi (TIMSK5,TOIE5);              // enable timer5 overflow detect
+} 
+
+//******************************************************************
+// Timer5 setup
+// set prscaler to 1,  Phase Correct PWM
+
+void Setup_Timer5() {
+
+// Timer5 Clock Prescaler to : 1
+  sbi (TCCR5B, CS50);  // set
+  cbi (TCCR5B, CS51);  // clear
+  cbi (TCCR5B, CS52);
+  
+  sbi (TCCR5A, WGM50);   // Mode 1, phase correct PWM
+  sbi (TCCR5A, WGM51);
+  cbi (TCCR5B, WGM52);
+  cbi (TCCR5B, WGM53);
+
+  sbi (TIMSK5,TOIE5);    // enable overflow detect
+  
+}
+
+// timer1 setup  (sets pins 9 and 10)
+// set prscaler to 1, PWM mode to phase correct PWM,  16000000/510 = 31372.55 Hz clock
+void Setup_Timer3() {
+
+// Timer1 Clock Prescaler to : 1
+  sbi (TCCR3B, CS30);
+  cbi (TCCR3B, CS31);
+  cbi (TCCR3B, CS32);
+
+  // Timer1 PWM Mode set to Phase Correct PWM
+  sbi (TCCR3A, COM3A0);  // set OC3A on Compare Match, PWM pin 5
+  sbi (TCCR3A, COM3A1);
+  sbi (TCCR3A, COM3B0);  // set OC3B on Compare Match, PWM pin 2
+  sbi (TCCR3A, COM3B1);
+  sbi (TCCR3A, COM3C0);  // set OC3C on Compare Match, PWM pin 3
+  sbi (TCCR3A, COM3C1);
+
+  sbi (TCCR3A, WGM30);  // Mode 1  / phase correct PWM
+  sbi (TCCR3A, WGM31);
+  cbi (TCCR3B, WGM32);
+  cbi (TCCR3B, WGM33);
+}
+
+void Setup_Timer4() {
+
+// Timer1 Clock Prescaler to : 1
+  sbi (TCCR4B, CS40);
+  cbi (TCCR4B, CS41);
+  cbi (TCCR4B, CS42);
+
+  // Timer1 PWM Mode set to Phase Correct PWM
+  sbi (TCCR4A, COM4A0);  // set OC4A on Compare Match, PWM pin 6
+  sbi (TCCR4A, COM4A1);
+  sbi (TCCR4A, COM4B0);  // set OC4B on Compare Match, PWM pin 7
+  sbi (TCCR4A, COM4B1);
+  sbi (TCCR4A, COM4C0);  // set OC4C on Compare Match, PWM pin 8
+  sbi (TCCR4A, COM4C1);
+
+  sbi (TCCR4A, WGM40);  // Mode 1  / phase correct PWM
+  sbi (TCCR4A, WGM41);
+  cbi (TCCR4B, WGM42);
+  cbi (TCCR4B, WGM43);
+}
+
+
+//******************************************************************
+// Timer5 Interrupt Service at 31372/4 KHz = 128uSec
+// this is the timebase REFCLOCK for the DDS generator
+// runtime : 8 microseconds ( inclusive push and pop)
+// OC2A - pin 11
+// OC1B - pin 10
+// OC1A - pin 9
+// https://www.arduino.cc/en/Tutorial/SecretsOfArduinoPWM
+ISR(TIMER5_OVF_vect) {
+  //use global variable with interrupt -> use volatile variable
+  //float _scalingFactor = scalingFactor; //For optimization purpose because volatile variable would be load every time
+  
+  sigma = sigma + delta; // soft DDS, phase accu with 32 bits
+  
+  phaseX = sigma >> 24; 
+  int valueX = pgm_read_word_near(SINE_VALUE + phaseX);
+  int X = (valueX == 0) ? 0 : valueX*scalingFactor;
+  
+  phaseIX = phaseX + 128;
+  int valueIX = pgm_read_word_near(SINE_VALUE + phaseIX);
+  int IX = (valueIX == 0) ? 0 : valueIX*scalingFactor;
+  
+  phaseY = phaseX + 85 ;
+  int valueY = pgm_read_word_near(SINE_VALUE + phaseY);
+  int Y = (valueY == 0) ? 0 : valueY*scalingFactor;
+  
+  phaseIY = phaseY + 128;
+  int valueIY = pgm_read_word_near(SINE_VALUE + phaseIY);
+  int IY = (valueIY == 0) ? 0 : valueIY*scalingFactor;
+  
+  phaseZ = phaseX + 170;
+  int valueZ = pgm_read_word_near(SINE_VALUE + phaseZ);
+  int Z = (valueZ == 0) ? 0 : valueZ*scalingFactor;
+  
+  phaseIZ = phaseZ + 128;
+  int valueIZ = pgm_read_word_near(SINE_VALUE + phaseIZ);
+  int IZ = (valueIZ == 0) ? 0 : valueIZ*scalingFactor;
+  
+  switch(mode)
+  {
+    case FAULT:
+    {
+      OCR3A=1023;  // pwm pin 5
+      OCR3B=1023;  // pwm pin 2
+      OCR3C=1023;  // pwm pin 3
+      OCR4A=1023;  // pwm pin 6
+      OCR4B=1023;  // pwm pin 7
+      OCR4C=1023;  // pwm pin 8
+      break;
+    }
+    case HALF_WAVE:
+    {
+      OCR3A=1023 - X;  // pwm pin 5
+      OCR3B=1023 - IX;  // pwm pin 2
+      OCR3C=1023;  // pwm pin 3
+      OCR4A=1023;  // pwm pin 6
+      OCR4B=1023;  // pwm pin 7
+      OCR4C=1023;  // pwm pin 8
+      break;
+    }
+    case FULL_WAVE:
+    {
+      OCR3A=1023 - X;  // pwm pin 5
+      OCR3B=1023 - IX;  // pwm pin 2
+      OCR3C=1023 - X;  // pwm pin 3
+      OCR4A=1023 - IX;  // pwm pin 6
+      OCR4B=1023;  // pwm pin 7
+      OCR4C=1023;  // pwm pin 8
+      break;
+    }
+    case THREE_PHASE:
+    {
+      OCR3A=1023 - X;  // pwm pin 5
+      OCR3B=1023 - IX;  // pwm pin 2
+      OCR3C=1023 - Y;  // pwm pin 3
+      OCR4A=1023 - IY;  // pwm pin 6
+      OCR4B=1023 - Z;  // pwm pin 7
+      OCR4C=1023 - IZ;  // pwm pin 8
+      break;
+    }
+  }
+  //cbi(PORTD,testPin);            
+  
+}
